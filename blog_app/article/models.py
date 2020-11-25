e8385d550e3b58b723d056625904d118db073411
Sud <shindesudarshan1997@gmail.com>
2020-11-26 00:28:20 +0530

    Initial commit

---
+++ blog_app/article/models.py
@@ -0,0 +1,32 @@
+from django.contrib.auth.models import User
+from django.db import models
+#from ckeditor.fields import RichTextField
+# Create your models here.
+
+class Article(models.Model):
+    author = models.ForeignKey(User,on_delete = models.CASCADE,verbose_name = "Author")
+    title = models.CharField(max_length = 50,verbose_name = "Title")
+    content = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True,verbose_name="Created At")
+    article_image = models.FileField(blank = True,null = True,verbose_name="Article image")
+    slug = models.SlugField(unique=True, max_length=100)
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        return super(Article, self).save(*args, **kwargs)
+
+    class Meta:
+        ordering = ['-created_date']
+class Comment(models.Model):
+    article = models.ForeignKey(Article,on_delete = models.CASCADE,verbose_name = "Article",related_name="comments")
+    comment_author = models.CharField(max_length = 50,verbose_name = "Comment Author")
+    comment_content = models.CharField(max_length = 200,verbose_name = "Content")
+    comment_date = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.comment_content
+    class Meta:
+        ordering = ['-comment_date']
